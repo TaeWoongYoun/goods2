eecac6003118c841e9c4d5740acacf04de84bba6
xodnd0507 <24457545yong@gmail.com>
2024-03-12 15:32:02 +0900

    test : Composite button function successful

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 $(document).ready(function () {
-    // 추가버튼 클릭 시 파일 입력 창 트리거
     $("#uploadButton").on("click", function () {
         $("#fileInput").trigger("click");
     });
 
-    // 파일이 선택되었을 때 동작하는 이벤트
     $("#fileInput").on("change", function () {
         var file = $(this)[0].files[0];
 
-        // 선택된 파일을 읽어서 미리보기 표시 (옵션)
         var reader = new FileReader();
         reader.onload = function (e) {
-            // 이미지를 표시할 수 있는 경우에만 미리보기 표시
-            $(".editarea").html('<img src="' + e.target.result + '" alt="업로드된 이미지" style="max-width: 100%; max-height: 100%;" />');
+            $("#contentContainer").html('<img src="' + e.target.result + '" alt="업로드된 이미지" style="max-width: 100%; max-height: 100%;" />');
+            $("#textInput").val("");
         };
         reader.readAsDataURL(file);
     });
-});
 
-$(document).ready(function () {
     $("#textButton").on("click", function () {
         $("#textInput").toggle().focus();
     });
 
-    $("#textInput").on("blur", function () {
-        var text = $(this).val();
-        $(".editarea").append('<div class="textOverlay">' + text + '</div>');
-        $(this).hide().val("");
+    $("#mergeButton").on("click", function () {
+        var text = $("#textInput").val();
+        drawTextOnImage(text);
+        $("#textInput").hide().val("");
     });
 
-    $("#uploadButton").on("click", function () {
-        // 파일 업로드 로직 추가
-        var imageUrl = "https://via.placeholder.com/150"; // 예시 이미지 URL
-        $(".editarea").append('<img src="' + imageUrl + '" alt="Uploaded Image" class="uploadedImage">');
-    });
+    function drawTextOnImage(text) {
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext("2d");
+
+        var img = document.querySelector('.editarea img');
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        context.font = "20px Arial";
+        context.fillStyle = "white";
+        context.textAlign = "center";
+
+        context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+        $("#contentContainer").html('<img src="' + canvas.toDataURL("image/png") + '" alt="업로드된 이미지" style="max-width: 100%; max-height: 100%;" />');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
 });
